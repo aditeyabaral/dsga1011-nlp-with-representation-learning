ea38ad49cbe8c017448d1c6e9eeb315911f9152f
Aditeya Baral <ab12057@b-3-16.c.hpc-slurm-9c75.internal>
2024-10-08 02:50:54 -0400

    Generate outfiles for beam search

--- hw2/code/run_beam_search.sh
+++ hw2/code/run_beam_search.sh
@@ -1,5 +1,10 @@
 for i in {1..5}
 do
-    echo "Running beam_size = $i"
+    echo "Running beam_size = $i at $(date)"
+    start=$(date +%s)
     python main.py --beam_search --beam_size $i > out_beam_$i.txt
+    end=$(date +%s)
+    elapsed=$((end-start))
+    echo "Completed beam_size = $i in $((elapsed/60)) minutes"
 done
+
