d1c4109d4866f4bb04873a56040f323849eae8dd
aditeyabaral <aditeya.baral@gmail.com>
2024-10-12 00:15:58 -0400

    Reset batch size

--- hw3/code/main.py
+++ hw3/code/main.py
@@ -198,7 +198,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--learning_rate", type=float, default=5e-5)
     parser.add_argument("--num_epochs", type=int, default=3)
-    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=8)
 
     args = parser.parse_args()
 
