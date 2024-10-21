590851599632a1b8f68f4546f3c83ebcdd4c539d
aditeyabaral <aditeya.baral@gmail.com>
2024-10-21 12:27:27 -0400

    Add script to push models to hub

---
+++ hw3/code/upload_model_hf.py
@@ -0,0 +1,27 @@
+import argparse
+from transformers import AutoModelForSequenceClassification
+
+parser = argparse.ArgumentParser("Upload model to HuggingFace Hub")
+
+parser.add_argument(
+    "--model-dir",
+    "-d",
+    type=str,
+    default="./out",
+    help="Directory where model is saved",
+)
+parser.add_argument(
+    "--model-name",
+    "-n",
+    type=str,
+    help="Name of the model to be uploaded",
+    required=True,
+)
+
+args = parser.parse_args()
+
+
+model = AutoModelForSequenceClassification.from_pretrained(args.model_dir)
+model.push_to_hub(
+    repo_id=args.model_name, private=True, token="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"
+)
