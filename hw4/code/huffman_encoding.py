f00453b27f7b38f7e57d56ab43892a9fd5624789
aditeyabaral <aditeya.baral@gmail.com>
2024-11-13 23:49:44 -0500

    Add huffman tree visualisation

--- hw4/code/huffman_encoding.py
+++ hw4/code/huffman_encoding.py
@@ -1,5 +1,9 @@
+from ete3 import Tree
+import matplotlib.pyplot as plt
+
+
 class Node:
-    def __init__(self, value, prob):
+    def __init__(self, value: str, prob: float):
         self.value = value
         self.prob = prob
         self.left = None
@@ -13,9 +17,9 @@ class Node:
 
 class PriorityQueue:
     def __init__(self):
-        self.queue = []
+        self.queue: list[Node] = []
 
-    def insert(self, node):
+    def insert(self, node: Node):
         self.queue.append(node)
         self.queue.sort(key=lambda node: node.prob, reverse=True)
 
@@ -29,21 +33,23 @@ class PriorityQueue:
         return len(self.queue)
 
 
-def create_huffman_tree(priority_queue):
+def create_huffman_tree(priority_queue: PriorityQueue):
     while len(priority_queue) > 1:
-        left = priority_queue.pop()
-        right = priority_queue.pop()
-        parent = Node(f"{left.value}-{right.value}", left.prob + right.prob)
-        # print(f"Parent: {parent}, Left: {left}, Right: {right}")
-        parent.left = left
-        parent.right = right
+        node_1 = priority_queue.pop()
+        node_2 = priority_queue.pop()
+        parent = Node(f"{node_1.value}+{node_2.value}", node_1.prob + node_2.prob)
+        left_node, right_node = (
+            (node_1, node_2) if node_1.prob < node_2.prob else (node_2, node_1)
+        )
+        parent.left = left_node
+        parent.right = right_node
         priority_queue.insert(parent)
     root = priority_queue.pop()
     label_huffman_tree(root)
     return root
 
 
-def label_huffman_tree(node):
+def label_huffman_tree(node: Node):
     if node is None:
         return
     if node.left is not None:
@@ -54,16 +60,23 @@ def label_huffman_tree(node):
         label_huffman_tree(node.right)
 
 
-def display_huffman_tree(node, depth=0):
-    # print the tree in a readable format
-    if node is None:
-        return
-    display_huffman_tree(node.right, depth + 1)
-    print("    " * depth + str(node))
-    display_huffman_tree(node.left, depth + 1)
+def plot_huffman_tree(node: Node):
+    def add_node_to_tree(node: Node, tree: Tree):
+        if node is None:
+            return
+        tree.add_child(name=f"(v={node.value}, p={node.prob})")
+        add_node_to_tree(node.left, tree.children[-1])
+        add_node_to_tree(node.right, tree.children[-1])
+
+    tree = Tree()
+    tree.add_child(name=f"(v={node.value}, p={node.prob})")
+    add_node_to_tree(node.left, tree.children[-1])
+    add_node_to_tree(node.right, tree.children[-1])
+    print(tree)
+    tree.render("huffman_tree.png", dpi=300, tree_style=None)
 
 
-def encode_token(token, node, code=""):
+def encode_token(token: str, node: Node, code=""):
     if node is None:
         return ""
     if node.value == token:
@@ -90,11 +103,9 @@ if __name__ == "__main__":
     priority_queue = PriorityQueue()
     for word, freq in word2freq.items():
         priority_queue.insert(Node(word, freq / total_frequency))
-    # print(f"Initial Queue:\n{priority_queue}")
 
     huffman_tree = create_huffman_tree(priority_queue)
-    print("\nHuffman Tree:")
-    display_huffman_tree(huffman_tree)
+    plot_huffman_tree(huffman_tree)
 
     print("\nHuffman Codes:")
     for word in word2freq:
